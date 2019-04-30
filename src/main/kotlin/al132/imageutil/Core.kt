8743f00893d413f33a41beb4c221c1898ec31988
al132mc <apbertram1@gmail.com>
2019-04-30 13:14:12 -0500

    v1.0.1

--- src/main/kotlin/al132/imageutil/Core.kt
+++ src/main/kotlin/al132/imageutil/Core.kt
@@ -1,39 +1,61 @@
-package main.java.al132.imageutil
+package al132.imageutil
 
 import java.awt.Color
 import java.awt.Font
 import java.awt.Graphics2D
 import java.awt.image.BufferedImage
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
 import javax.imageio.ImageIO
 
-val startX = 0
+
+val absolutePath = File("").absolutePath
+val resourceRoot = "resources/alchemistry"
+val imageResource = ClassLoader.getSystemClassLoader().getResource("element_big.png");
 
 fun main() {
-    val elementData = File("elements.csv").readLines().drop(2)
 
+    println("Please enter the name of your element")
+    val elementName = readLine()!!.toLowerCase()
+    if (elementName.startsWith("/csv")) runCSV(elementName)
+    else {
+        println("Please enter the abbreviation of your element")
+        val abbreviation = readLine()!!.toLowerCase().capitalize()
+        println("Please enter the R,G,B of your element, example in next line")
+        println("230, 114, 52")
+        val (r, g, b) = readLine()!!.split(",").map { it.trim().toInt() }
+        val buf: BufferedImage = ImageIO.read(imageResource)
+        processImage(buf, abbreviation, elementName, r, g, b)
+        generateJSON(elementName)
+    }
+    println("Resources generated!")
+}
+
+fun runCSV(rawInput: String) {
+    val elementData = File(rawInput.split(" ")[1]).readLines().drop(2)
     for (line in elementData) {
-        var buf: BufferedImage = ImageIO.read(File("element_big.png"))
+        val buf: BufferedImage = ImageIO.read(imageResource)
         val (elementName, abbrev, r, g, b) = line.split(",").drop(1).take(5)
         processImage(buf, abbrev, elementName, r.toInt(), g.toInt(), b.toInt())
         generateJSON(elementName)
     }
 }
 
+
 fun generateJSON(name: String) {
-    if (!Files.exists(Paths.get("json"))) Files.createDirectory(Paths.get("json"))
+    val path = File("$absolutePath/$resourceRoot/models/item/element_$name.json")
+    path.parentFile.mkdirs()
     val output = """{
 	"parent": "item/generated",
 	 "textures": {
 		"layer0": "alchemistry:items/elements/element_$name"
 	}
 }""".trimIndent()
-    File("json/element_$name.json").writeText(output)
+    path.writeText(output)
 }
 
 fun processImage(old: BufferedImage, abbreviation: String, name: String, r: Int, g: Int, b: Int) {
+    val startX = 0
+
     val temp = BufferedImage(old.width, old.height, BufferedImage.TRANSLUCENT)
     val graphics: Graphics2D = temp.createGraphics()
     graphics.drawImage(tint(old, r, g, b), 0, 0, old.width, old.height, null);
@@ -42,10 +64,11 @@ fun processImage(old: BufferedImage, abbreviation: String, name: String, r: Int,
     graphics.drawString(abbreviation, startX + 1, 11)
     graphics.paint = Color(225, 225, 225)
     graphics.drawString(abbreviation, startX, 10)
-
     graphics.dispose()
-    if (!Files.exists(Paths.get("textures"))) Files.createDirectory(Paths.get("textures"))
-    ImageIO.write(temp, "png", File("textures/element_$name.png"))
+
+    val graphicsFile = File("$absolutePath/$resourceRoot/textures/items/elements/element_$name.png")
+    graphicsFile.parentFile.mkdirs()
+    ImageIO.write(temp, "png", graphicsFile)
 }
 
 fun tint(image: BufferedImage, r: Int, g: Int, b: Int): BufferedImage {
